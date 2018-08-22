6add4ad8ff427dd93bb768db7c08e2e9ddca51b0
Kevin Gagnon <klg73@outlook.com>
2018-08-22 08:10:33 -0400

    Added an event

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-events',
-  template: `
-    <h2>Hello World</h2>
-    <img src="/assets/images/basic-shield.png"/>
-    `
+  template: '<app-eventslist></app-eventslist>'
 })
 export class EventsAppComponent {
   title = 'app';
