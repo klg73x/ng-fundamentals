393afa6d5d5cc20727b7014ab5522da9200f4ebe
Kevin Gagnon <klg73@outlook.com>
2018-08-22 10:50:13 -0400

    Added some styles

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-eventthumbnail',
@@ -10,11 +10,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         <div>Price: \${{event.price}}</div>
         <div>
            <span>Location: {{event.location.address}}</span>
-           <span>&nbsp;</span>
-           <span>{{event.location.city}}, {{event.location.country}}</span>
+           <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
         </div>
     </div>
-    `
+    `,
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
