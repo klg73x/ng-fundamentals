0c55fb97156f15e044d90ddad54e403f3df00d5b
Kevin Gagnon <klg73@outlook.com>
2018-08-22 09:27:46 -0400

    updated to a details component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-eventslist',
-    templateUrl: 'events-list.component.html'
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr/>
+        <app-eventthumbnail [event]="event1"></app-eventthumbnail>
+    </div>
+    `
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2018',
