8b0a4855b56580e919da76039e81bc3343fd4448
Kevin Gagnon <klg73@outlook.com>
2018-08-29 21:59:31 -0400

    Added ngStyles and worked through ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input } from '@angular/core';
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
         <div>Price: \${{event?.price}}</div>
         <div *ngIf="event?.location">
            <span>Location: {{event?.location?.address}}</span>
@@ -25,4 +30,10 @@ import { Component, Input } from '@angular/core';
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeStyle() {
+        if (this.event && this.event.time === '8:00 am') {
+            return {color: '#003300', 'font-weight': 'bold'};
+        }
+    }
 }
