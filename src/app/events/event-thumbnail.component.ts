c0d39dc7705dd9c13387a4a5576f7b4f185abe7b
Kevin Gagnon <klg73@outlook.com>
2018-09-10 14:43:05 -0400

    added event model (interface) to the project

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IEvent } from './shared/index';
 
 @Component({
     selector: 'app-eventthumbnail',
@@ -29,7 +30,7 @@ import { Component, Input } from '@angular/core';
     `]
 })
 export class EventThumbnailComponent {
-    @Input() event: any;
+    @Input() event: IEvent;
 
     getStartTimeStyle() {
         if (this.event && this.event.time === '8:00 am') {
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared/index';
 
 declare let toastr;
 @Component({
@@ -19,7 +20,7 @@ declare let toastr;
     `
 })
 export class EventsListComponent implements OnInit {
-    events: any[];
+    events: IEvent[];
 
     constructor(private eventService: EventService, private toastrService: ToastrService,
         private route: ActivatedRoute) {
