4230648ade9b521674ebbbea66650ccc174ea854
Kevin Gagnon <klg73@outlook.com>
2018-09-17 10:48:02 -0400

    added a function that was a custom validator

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ISession } from '../shared/index';
+import { ISession, restrictedWords } from '../shared/index';
+
 
 @Component({
     templateUrl: 'create-session.component.html',
@@ -33,7 +34,7 @@ export class CreateSessionComponent implements OnInit {
         this.level = new FormControl('', Validators.required);
         this.abstract = new FormControl('', [
             Validators.required,
-            Validators.maxLength(400)
+            Validators.maxLength(400), restrictedWords(['foo', 'bar'])
         ]);
 
         this.newSessionForm = new FormGroup({
@@ -44,6 +45,7 @@ export class CreateSessionComponent implements OnInit {
             abstract: this.abstract
         });
     }
+
 
     saveSession(formValues) {
         const session: ISession = {
