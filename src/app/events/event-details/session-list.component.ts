d41b2f2aa29516fd9350620e1345a9d4bc5c3d79
Kevin Gagnon <klg73@outlook.com>
2018-09-19 10:27:36 -0400

    Sorting complete on Sessions List

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,12 +8,30 @@ import { ISession } from '../shared';
 export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[];
     @Input() filterBy: string;
+    @Input() sortBy: string;
     visibleSessions: ISession[] = [];
 
     ngOnChanges() {
         if (this.sessions) {
             this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(this.sortByNameAsc) :
+                this.visibleSessions.sort(this.sortByVotesDesc);
+
         }
+    }
+
+    sortByNameAsc(s1: ISession, s2: ISession) {
+        if (s1.name > s2.name) {
+            return 1;
+        } else if (s1.name === s2.name) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    sortByVotesDesc(s1: ISession, s2: ISession) {
+       return s2.voters.length - s1.voters.length;
     }
 
     filterSessions(filter: string) {
